29a240148bef49ebe70e3377067c3308b66b9a28
regrif-07 <regrif07.dev@gmail.com>
2024-11-17 23:49:59 +0100

    error-handling library created; error codes enumeration added

---
+++ error_handling/include/error_codes.h
@@ -0,0 +1,13 @@
+//
+// Created by regrif07 on 11/15/24.
+//
+
+#pragma once
+
+typedef enum
+{
+    NO_ERROR = 0,
+    ERROR_GENERAL = -1,
+    ERROR_INVALID_ARGUMENT = -2,
+    ERROR_ARGUMENT_OUT_OF_RANGE = -3,
+} ErrorCode;
