b6da97697105d34856eda2261d05b8992a601653
regrif-07 <regrif07.dev@gmail.com>
2024-11-22 18:19:09 +0100

    redundant functionality deleted from the string_utility module

--- tests/src/test_string_utility.c
+++ tests/src/test_string_utility.c
@@ -1,44 +1,9 @@
 #include <unity.h>
 
-#include <error_codes.h>
 #include <string_utility.h>
 
-#include <stdlib.h>
-#include <string.h>
-
-DEFINE_TO_STRING_CONVERSION_FUNCTION(int, int, "%d")
-DEFINE_TO_STRING_CONVERSION_FUNCTION(long long, longLong, "%lld")
-
-void setUp(void) {}
-void tearDown(void) {}
-
-void testIntToStringValid(void)
-{
-    ErrorCode errorCode;
-    int outStringLength;
-    char* result = intToString(42, &outStringLength, &errorCode);
-
-    TEST_ASSERT_EQUAL(NO_ERROR, errorCode);
-    TEST_ASSERT_NOT_NULL(result);
-    TEST_ASSERT_EQUAL_STRING("42", result);
-    TEST_ASSERT_EQUAL(2, outStringLength);
-
-    free(result);
-}
-
-void testLongLongToStringValid(void)
-{
-    ErrorCode errorCode;
-    int outStringLength;
-    char* result = longLongToString(123456789012345LL, &outStringLength, &errorCode);
-
-    TEST_ASSERT_EQUAL(NO_ERROR, errorCode);
-    TEST_ASSERT_NOT_NULL(result);
-    TEST_ASSERT_EQUAL_STRING("123456789012345", result);
-    TEST_ASSERT_EQUAL(15, outStringLength);
-
-    free(result);
-}
+void setUp() {}
+void tearDown() {}
 
 void testIsWhitespaceOnlyAllWhitespace(void)
 {
@@ -63,12 +28,6 @@ void testIsWhitespaceOnlyNullptr(void)
 int main(void)
 {
     UNITY_BEGIN();
-
-    // tests for intToString
-    RUN_TEST(testIntToStringValid);
-
-    // tests for longLongToString
-    RUN_TEST(testLongLongToStringValid);
 
     // tests for isWhitespaceOnly
     RUN_TEST(testIsWhitespaceOnlyAllWhitespace);
--- utility/include/string_utility.h
+++ utility/include/string_utility.h
@@ -1,29 +1,5 @@
 #pragma once
 
-/// Define a new to-string conversion function for TYPE.
-/// snprintf()-based conversion.
-/// @param TYPE type of value that will be converted to string
-/// @param TYPE_NAME name of type that will be used in the function name generation.
-/// @param FORMAT_SPECIFIER format specifier for the provided TYPE (%d for int, for example).
-/// Will be used in snprintf().
-#define DEFINE_TO_STRING_CONVERSION_FUNCTION(TYPE, TYPE_NAME, FORMAT_SPECIFIER)       \
-    char* TYPE_NAME##ToString(TYPE value, int* outStringLength, ErrorCode* errorCode) \
-    {                                                                                 \
-        const int stringLength = snprintf(nullptr, 0, FORMAT_SPECIFIER, value);       \
-        char* string = malloc(sizeof(char) * stringLength + 1);                       \
-        if (!string)                                                                  \
-        {                                                                             \
-            if (errorCode) *errorCode = ERROR_MEMORY;                                 \
-            return nullptr;                                                           \
-        }                                                                             \
-                                                                                      \
-        sprintf(string, FORMAT_SPECIFIER, value);                                     \
-                                                                                      \
-        if (outStringLength) *outStringLength = stringLength;                         \
-        if (errorCode) *errorCode = NO_ERROR;                                         \
-        return string;                                                                \
-    }                                                                                 \
-
 /// Check if the specified string is whitespace only.
 /// @param string string to check.
 /// @return true in case any non-whitespace character was found; false otherwise.
